bce42513c5c3420feff206e90f58a53dd5af3ab9
Aleksandr Salnikov <TaynbIy@gmail.com>
2024-08-28 14:31:30 +0500

    1431

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,45 +1,43 @@
-import styles from './page.scss'
 import Banner from './components/banner/Banner'
-import News from './components/news/News'
 import Navigation from './components/navigationCards/Navigation'
+import News from './components/news/News'
+import styles from './page.scss'
 
 export const revalidate = 60
 
-
 export const metadata = {
 	title: 'Главная',
-	description:'Добро пожаловать на главную страницу сайта Менеджмента ТФ МГУ! На сайте вы найдёте много полезных материалов, которые помогут как студентам, так и тем, кто интересуется направлением Менеджмент в Ташкентском Филиале МГУ',	
+	description:
+		'Добро пожаловать на главную страницу сайта Менеджмента ТФ МГУ! На сайте вы найдёте много полезных материалов, которые помогут как студентам, так и тем, кто интересуется направлением Менеджмент в Ташкентском Филиале МГУ',
 }
 
 export default function Home() {
-
-	
 	const params = [
 		{
 			width: '1060',
-			link:'/images/pictures/mainBuilding.jpg',
-			title:'Университет', 
-			href:'/university'
+			link: '/images/pictures/mainBuilding.jpg',
+			title: 'Университет',
+			href: '/university',
 		},
 		{
 			width: '645',
-			link:'/images/pictures/for-student.jpg',
-			title:'Студентам', 
-			href:'/for-students'
+			link: '/images/pictures/for-student.jpg',
+			title: 'Студентам',
+			href: '/for-students',
 		},
 		{
 			width: '400',
-			link:'/images/pictures/entrance.jpg',
-			title:'Абитуриентам', 
-			href:'/entrance'
+			link: '/images/pictures/entrance.jpg',
+			title: 'Абитуриентам',
+			href: '/entrance',
 		},
 	]
 
 	return (
 		<main className={styles.main}>
-			<Banner src='mainBuilding' title='МГУ в Ташкенте, факультет Менеджмент'/>
-			<News/>
-			<Navigation params={params}/>
+			<Banner src='mainBuilding' title='МГУ в Ташкенте, факультет Менеджмент' />
+			<News />
+			<Navigation params={params} />
 		</main>
 	)
 }
