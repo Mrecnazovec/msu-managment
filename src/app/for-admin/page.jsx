3e1cb7b5e50bac711d0ada57dc04b7dfa83978a8
Aleksandr Salnikov <TaynbIy@gmail.com>
2024-08-31 01:24:27 +0500

    0124

--- src/app/for-admin/page.jsx
+++ src/app/for-admin/page.jsx
@@ -9,6 +9,8 @@ import Link from 'next/link'
 import Image from 'next/image'
 import Breadcrumbs from '../components/breadcrumbs/Breadcrumbs'
 
+export const revalidate = 60
+
 const ForAdmin = () => {
 
 	const { data: session, status } = useSession()
