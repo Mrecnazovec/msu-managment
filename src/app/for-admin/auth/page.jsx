0c1cda11c2da7545eed95650e932fa6bb701bfcd
Aleksandr Salnikov <TaynbIy@gmail.com>
2024-08-31 20:11:28 +0500

    2011

--- src/app/for-admin/auth/page.jsx
+++ src/app/for-admin/auth/page.jsx
@@ -8,7 +8,6 @@ import Loading from '@/app/loading'
 
 
 export default function SignIn() {
-	const { data: session, status } = useSession()
 
 	const [login, setLogin] = useState('')
 	const [password, setPassword] = useState('')
@@ -37,14 +36,7 @@ export default function SignIn() {
 	}
 
 	
-	if (status === 'loading') {
-		return <Loading />
-	}
 
-	if (status === 'authenticated') {
-		redirect('/for-admin')
-		return null
-	}
 
 	return (
 		<main className='auth'>
